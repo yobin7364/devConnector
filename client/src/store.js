79e983bfd2d9a72d93903c2916907ec466eb9d99
yobin kumar pun <punyobin123@gmail.com>
2020-06-15 22:50:20 +0545

    redux dev tool change

--- client/src/store.js
+++ client/src/store.js
@@ -10,6 +10,6 @@ const middleware = [thunk];
 const store = createStore(rootReducer, 
     initialState, 
     compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f));
 
 export default store;
