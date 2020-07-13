c85aaffc696c33df975697dfaa878b03f2aa96ba
yobin kumar pun <punyobin123@gmail.com>
2020-07-13 23:29:39 +0545

    pagination for profile

--- client/src/components/profiles/Profiles.jsx
+++ client/src/components/profiles/Profiles.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import Spinner from '../common/Spinner';
 import {getProfiles} from '../../actions/profileAction';
 import ProfileItem from './ProfileItem';
-
+import ReactPaginate from 'react-paginate';
+import './style.css';
 
 class Profiles extends Component {
 
@@ -12,8 +13,14 @@ class Profiles extends Component {
         super();
     
         this.state = {
-          search:null
+          search: null,
+          data: [],
+          offset: 0,
+          perPage: 1,
+          currentPage: 0
         };
+
+        this.handlePageClick = this.handlePageClick.bind(this);
       }
 
     searchSpace = (event) => {
@@ -26,33 +33,71 @@ class Profiles extends Component {
         this.props.getProfiles();
     }
 
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+  
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        });
+  
+    };
+
     render() {
         const {profiles, loading} = this.props.profile;
         let profileItems;
+        var pagination,pageCount;
 
         if(profiles === null || loading) {
             profileItems = <Spinner/>
         }
+
         else{
             if(profiles.length > 0){
                 //here we are making multiple components 'ProfileItem'
-                // profileItems = profiles.map(profile => (
-                //     <ProfileItem key={profile._id} profile={profile} />
+                
+                if(this.state.search){
+                    profileItems = profiles.filter((data) => {
 
-                profileItems = profiles.filter((data) => {
-
-                    if(this.state.search == null){
-                        return data;
-                    }
-
-                    if(data.user.name.toLowerCase().includes(this.state.search.toLowerCase())){
-                        return data;
-                    }
-                }).map( profile => 
-                    (
-                        <ProfileItem key={profile._id} profile={profile} />
+                        if(data.user.name.toLowerCase().includes(this.state.search.toLowerCase())){
+                            return data;
+                        }
+                        
+                    }).map( profile => 
+                        (   
+                            <ProfileItem key={profile._id} profile={profile} />
+                        )
                     )
-                )
+                }      
+                
+                else{
+                    
+                    const slice = profiles.slice(this.state.offset, this.state.offset + this.state.perPage);
+                    profileItems = slice.map(profile => (
+                        <ProfileItem key={profile._id} profile={profile} />
+                    ));
+
+                    pageCount = Math.ceil(profiles.length / this.state.perPage);
+
+                    pagination = (
+                        <ReactPaginate
+                                previousLabel={"prev"}
+                                nextLabel={"next"}
+                                breakLabel={"..."}
+                                breakClassName={"break-me"}
+                                pageCount={pageCount}
+                                marginPagesDisplayed={2}
+                                pageRangeDisplayed={5}
+                                onPageChange={this.handlePageClick}
+                                containerClassName={"pagination"}
+                                subContainerClassName={"pages pagination"}
+                                activeClassName={"active"}
+                            />
+                    );
+
+                }
+  
                 
             }
             else{
@@ -61,6 +106,7 @@ class Profiles extends Component {
         }
 
         return (
+            
             <div className="profiles">
                 <div className="container">
                     <div className="row">
@@ -73,6 +119,9 @@ class Profiles extends Component {
                             <input type="text" className="form-control w-50" placeholder="Search Profile" onChange={(e)=>this.searchSpace(e)} />
                             </div>
                             {profileItems}
+                            <div className="d-flex justify-content-center">
+                            {pagination}
+                            </div>
                         </div>
                     </div>
                 </div>
