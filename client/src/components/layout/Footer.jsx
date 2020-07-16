6c7023c1308171a3149fdcea2bc5fafc5e562110
yobin kumar pun <punyobin123@gmail.com>
2020-07-16 11:18:46 +0545

    min with for nav bars

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <footer className="bg-dark text-white mt-5 p-4 text-center">
+            <footer className="bg-dark text-white mt-5 p-4 text-center" style={{ minWidth:"575px"}}>
                 Copyright &copy; 2018 Dev Connector
             </footer>
         )
